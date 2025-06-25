fe83386b6c05d25386771d02defc65e52dcc7559
Walter Daems <walter.daems2@uantwerpen.be>
2025-06-25 15:08:12 +0200

    Remove tmp files.

--- src/fw/main.py
+++ src/fw/main.py
@@ -14,21 +14,21 @@ if __name__ == "__main__" or __name__ == "fw.main":
     )
 
 
-    action = "train"
+    action = "visualize"
 
     # Ensure all environments are registered once
     initialize_all_environments()
 
     # Define simulator inputs
-    envs = [get_environment(py_sim_env_name)]
-            # get_environment(fh_sim_env_name)]
+    envs = [get_environment(fh_sim_env_name)]
+            # get_environment(py_sim_env_name)]
 
     # Create a Trainer and pass on a list of agents that you want to train
     trainer = Trainer(PPO_POLICY_NAME, envs, "SimpleScheduler")
 
     if "train" == action:
         configuration = {py_sim_env_name: StopCondition(stop_criterion=Criterion.TARGET_REWARD, target_reward=1.01, patience=3, max_time_steps=600),
-                         fh_sim_env_name: StopCondition(stop_criterion=Criterion.NBR_POLICY_UPDATES, max_time_steps=2000),
+                         fh_sim_env_name: StopCondition(stop_criterion=Criterion.TARGET_REWARD, target_reward=0.86, patience=3, max_time_steps=800),
                          lunar_env_name: StopCondition(stop_criterion=Criterion.TARGET_REWARD_AND_LOSS, target_reward=0.20, target_loss=0.8, patience=1, max_time_steps=300)}
         metrics = trainer.train(configuration)
 
