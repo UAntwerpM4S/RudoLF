33af5b940afd87de3fc2b33d4c03710bdc1e13f0
Walter Daems <walter.daems2@uantwerpen.be>
2026-01-07 15:40:59 +0100

    Re-introduce smooothen_action in FhSimEnv.

--- src/fw/simulators/fh_sim_env.py
+++ src/fw/simulators/fh_sim_env.py
@@ -136,6 +136,26 @@ class FhSimEnv(PySimEnv):
         self._fh_sim.reset()
 
 
+    def _smoothen_action(self, action: np.ndarray):
+        """Smoothen the action to prevent erratic behaviour of the ship.
+
+        Args:
+            action: Array of [rudder, thrust] commands
+        """
+        # Apply rate limiting to action changes
+        alpha = 0.3
+
+        if self.total_steps == 0:
+            turning_smooth = action[0]
+            thrust_smooth = abs(action[1])
+        else:
+            # Smooth action application
+            turning_smooth = alpha * action[0] + (1 - alpha) * self.performed_action[0]
+            thrust_smooth = alpha * abs(action[1]) + (1 - alpha) * self.performed_action[1]
+
+        return np.array([turning_smooth, thrust_smooth], dtype=np.float32)
+
+
     def _has_enough_keel_clearance(self, depth_threshold=0.5):
         return self._fh_sim.ship_interface.getKeelClearance() >= depth_threshold
 
